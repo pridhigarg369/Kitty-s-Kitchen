3042b4313ac440e32e545b32dd068a243a87fce8
pridhigarg369 <pridhi1667.be20@chitkara.edu.in>
2022-09-12 10:06:55 +0530

    icon

--- dham/dham.js
+++ dham/dham.js
@@ -1,35 +1,36 @@
 // -----------------------------Transparent to Solid header--------------------------------
-window.onscroll = function() {scrollFunction()};
-function scrollFunction(){
-    console.log("mepw");
-    var hide3 = document.getElementsByClassName("posfix")[0];   
-    // var show = document.getElementsByClassName("header")[0];
-    // var smlogo = document.getElementsByClassName("logo")[0];
-    var head_border = document.getElementById("Header-Border");
-    // var hdele = document.getElementsByClassName("header_element")[0];
-    // var bold1 = document.getElementsByClassName("bold");
-    // var fixabout = document.getElementsByClassName("div_background")[0];
-    if(document.body.scrollTop > 90 || document.documentElement.scrollTop > 90){
-        hide3.style.display= "block";
-        head_border.style.display="block";
-        // show.classList.add('posfix');
-        // smlogo.classList.add('fixlogo');
-        // hdele.classList.add('fixheader_element');
-        // fixabout.classList.add('fixabout');
-        // for(var i=0;i<bold1.length;i++){
-        //     bold1[i].classList.add('fixbold');
-        // }        
-    }
-    else{
-        hide3.style.display="none";
-        head_border.style.display="none";
-        // show.classList.remove('posfix');
-        // smlogo.classList.remove('fixlogo');
-        // hdele.classList.remove('fixheader_element');
-        // fixabout.classList.remove('fixabout');
-        // for(var i=0;i<bold1.length;i++){
-        //     bold1[i].classList.remove('fixbold');
-        // }
-    }
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  console.log("mepw");
+  // var hide3 = document.getElementsByClassName("posfix")[0];
+  // var show = document.getElementsByClassName("header")[0];
+  // var smlogo = document.getElementsByClassName("logo")[0];
+  var head_border = document.getElementById("Header-Border");
+  // var hdele = document.getElementsByClassName("header_element")[0];
+  // var bold1 = document.getElementsByClassName("bold");
+  // var fixabout = document.getElementsByClassName("div_background")[0];
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    hide3.style.display = "block";
+    head_border.style.display = "block";
+    // show.classList.add('posfix');
+    // smlogo.classList.add('fixlogo');
+    // hdele.classList.add('fixheader_element');
+    // fixabout.classList.add('fixabout');
+    // for(var i=0;i<bold1.length;i++){
+    //     bold1[i].classList.add('fixbold');
+    // }
+  } else {
+    hide3.style.display = "none";
+    head_border.style.display = "none";
+    // show.classList.remove('posfix');
+    // smlogo.classList.remove('fixlogo');
+    // hdele.classList.remove('fixheader_element');
+    // fixabout.classList.remove('fixabout');
+    // for(var i=0;i<bold1.length;i++){
+    //     bold1[i].classList.remove('fixbold');
+    // }
+  }
 }
 // ------------------------------------------------------------------------------------
